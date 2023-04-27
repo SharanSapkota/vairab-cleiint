0ec5d6a57afe3372e5e6a52171e51a729a9311b5
SharanSapkota <sapkotarambbo@gmail.com>
2023-04-27 15:41:44 +0545

    Initial set up

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+import UserRegistation from "./components/UserRegistration";
+import Login from "./components/Login";
+
+import Home from "./components/Home";
+
+function App() {
+  return (
+   <div>
+        <BrowserRouter>
+            <Routes>
+              <Route path="/home" element= { <Home/>} />
+              <Route path="/register" element= { <UserRegistation/>} />
+              <Route path="/" element= { <Login/>} />
+            </Routes>
+        </BrowserRouter>
+   </div>
+  );
+}
+
+export default App;
---
+++ testLogs.js
@@ -0,0 +1,62 @@
+const testLogs = [{
+  ip: '63.140.98.80',
+  user: '-',
+  timestamp: Date.now(),
+  method: 'GET',
+  path: '/blog/tags/puppet?flav=rss20',
+  protocol: 'HTTP/1.1',
+  status: '200',
+  size: '14872',
+  referer: 'http://www.semicomplete.com/blog/tags/puppet?flav=rss20',
+  userAgent: 'Tiny Tiny RSS/1.11 (http://tt-rss.org/)'
+},
+{
+  ip: '63.140.98.80',
+  user: '-',
+  timestamp: Date.now(),
+  method: 'GET',
+  path: '/blog/geekery/solving-good-or-bad-problems.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+semicomplete%2Fmain+%28semicomplete.com+-+Jordan+Sissel%29',
+  protocol: 'HTTP/1.1',
+  status: '200',
+  size: '10756',
+  referer: '-',
+  userAgent: 'Tiny Tiny RSS/1.11 (http://tt-rss.org/)'
+},
+{
+  ip: '66.249.73.135',
+  user: '-',
+  timestamp: Date.now(),
+  method: 'GET',
+  path: '/?flav=atom',
+  protocol: 'HTTP/1.1',
+  status: '200',
+  size: '32352',
+  referer: '-',
+  userAgent: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+},
+{
+  ip: '180.76.6.56',
+  user: '-',
+  timestamp: Date.now(),
+  method: 'GET',
+  path: '/robots.txt',
+  protocol: 'HTTP/1.1',
+  status: '200',
+  size: '-',
+  referer: '-',
+  userAgent: 'Mozilla/5.0 (Windows NT 5.1; rv:6.0.2) Gecko/20100101 Firefox/6.0.2'
+},
+{
+  ip: '46.105.14.53',
+  user: '-',
+  timestamp: Date.now(),
+  method: 'GET',
+  path: '/blog/tags/puppet?flav=rss20',
+  protocol: 'HTTP/1.1',
+  status: '200',
+  size: '14872',
+  referer: '-',
+  userAgent: 'UniversalFeedParser/4.2-pre-314-svn +http://feedparser.org/'
+}]
+
+export default testLogs;
