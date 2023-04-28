d7681cd83f100795c50ce38b7ab3418149bfebe0
SharanSapkota <sapkotarambbo@gmail.com>
2023-04-28 18:52:10 +0545

    Add table component call

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,104 +1,15 @@
-import {
-   EuiBasicTable,
-   EuiLink,
-} from '@elastic/eui';
-import testLogs from '../../../testLogs';
-import { Theming } from './barChart';
-   const users = testLogs;
-   const columns = [
-     {
-       field: 'ip',
-       name: 'IP address',
-       sortable: true,
-       truncateText: true,
-       header: false,
-       enlarge: true,
-       footer: ({ items }) => <span>{items.length} Ip</span>,
-         render: (user) => {
-            return (
-             `${user}`
-         )},
-     },
-     {
-       field: 'user',
-       name: 'User',
-       truncateText: true,
-       footer: ({ items }) => <span>{items.length} users</span>,
-       mobileOptions: {
-         show: false,
-       },
-     },
-     {
-       field: 'method',
-       name: 'Method',
-       render: (username) => (
-         <EuiLink href="#" target="_blank">
-           {username}
-         </EuiLink>
-       ),
-     },
-     {
-       field: 'protocol',
-       name: 'Protocol',
-       render: (dateOfBirth) =>
-         {return `${dateOfBirth}`}
-     },
-     {
-       field: 'userAgent',
-       name: 'userAgent',
-       truncateText: true,
-       textOnly: true,
-       render: (location) => {
-         console.log('location', location)
-         return `${location}`;
-       },
-     },
-     {
-       field: 'status',
-       name: 'Status',
-       dataType: 'boolean',
-       render: (online) => {
-         return  `${online}`;
-       },
-     },
-   ];
- 
-   const getRowProps = (user) => {
-     const { ip } = user;
-     return {
-       'data-test-subj': `row-${ip}`,
-       className: 'customRowClass',
-       onClick: () => {},
-     };
-   };
- 
-   const getCellProps = (
-     user,
-     column
-   ) => {
-     const { id } = user;
-     const { field } = column;
-     return {
-       className: 'customCellClass',
-       'data-test-subj': `cell-${id}-${String(field)}`,
-       textOnly: true,
-     };
-   };
- 
+import React, {useState, useEffect} from 'react';
+// import { BarChart } from './barChart';
+import Table from './table';
 
-function Home() {
-    return (
-     <div>
-     <EuiBasicTable
-       tableCaption="Basic Table"
-       items={users}
-       columns={columns}
-       rowProps={getRowProps}
-       cellProps={getCellProps}
-     />
-     <Theming />
-     </div>
-    );
-  }
+const Home = () => {
   
-  export default Home;
+  return (
+  <>
+    <Table />
+    {/* <BarChart/> */}
+    </>
+  )
+  }
+
+export default Home
