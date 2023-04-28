7607fa9314ee07a717b8f83915d27c3e01bc91f1
SharanSapkota <sapkotarambbo@gmail.com>
2023-04-28 21:28:40 +0545

    Remove unused code

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,13 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Navigate } from 'react-router-dom'
 
 const Protected = ({ children }) => {
-    const [ isAuthenticated, setIsAuthenticated ] = useState(false)
-    localStorage.getItem('token') && setIsAuthenticated(true)  
 
-  if (!isAuthenticated) {
-    return <Navigate to="/" replace />
-  }
   return children
 }
 export default Protected
