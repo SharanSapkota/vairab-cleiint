191c92614f9682c972a4e3fbab1ee842ff9fda62
SharanSapkota <sapkotarambbo@gmail.com>
2023-04-28 21:29:20 +0545

    Send logs to Table and BarChart component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,45 @@
 import React, {useState, useEffect} from 'react';
-// import { BarChart } from './barChart';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { getAggregatedLogs, getLogs } from '../../services';
+
+import  BarChart from './barChart';
 import Table from './table';
 
 const Home = () => {
-  
+  const navigate = useNavigate();
+ const [logs, setLogs] = useState([])
+ const [aggregatedLogs, setAggregatedLogs] = useState([])
+
+
+  useEffect( () => {
+    const isAuth = localStorage.getItem('token')
+    if(!isAuth){
+      navigate('/')
+    } else {
+      getLog()
+      getSpecificLogs()
+    }
+  }, [])
+ 
+  const getSpecificLogs = () => {
+    getAggregatedLogs().then(({result}) => {
+        setAggregatedLogs(result)
+    })
+  }
+
+  const getLog = async () => {
+    const { result } = await getLogs()
+    if(result){
+      setLogs(result)
+    }
+  }
+
+
   return (
   <>
-    <Table />
-    {/* <BarChart/> */}
+
+    <Table aggregatedLogs = {aggregatedLogs} logs={logs}/>
+    <BarChart aggregatedLogs = {aggregatedLogs} logs={logs}/>
     </>
   )
   }
