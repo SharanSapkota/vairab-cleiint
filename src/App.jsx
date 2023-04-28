7789990a18b3256c1975b19d24277b5c4d171de3
SharanSapkota <sapkotarambbo@gmail.com>
2023-04-28 21:28:26 +0545

    Import css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-
+import './App.css'
 import UserRegistation from "./components/UserRegistration";
 import Login from "./components/Login";
 
