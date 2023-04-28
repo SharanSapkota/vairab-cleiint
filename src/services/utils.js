b527719360660e1e25e2e8a62828b98d1e4f1a7e
SharanSapkota <sapkotarambbo@gmail.com>
2023-04-28 18:51:02 +0545

    Add api endpoints ENUMS

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,3 +1,11 @@
-export const BASE_URL = "http://localhost:7000";
+export const BASE_URL   = "http://localhost:3200";
 
-export const LOGIN_API = "api/user/login" 
+export const LOGIN_API  = "api/user/login";
+export const REGISTER_API  = "api/user/register";
+
+export const GET_LOGS   = "api/logs/getAllLogs"
+
+
+export const ADMIN = 'admin';
+export const APACHE = 'apache';
+export const NGINX = 'ngInx';
